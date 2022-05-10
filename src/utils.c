64adb0638963594e9d8fe84f85916f72e9626efd
yukikondo <kny_>
2022-05-10 22:08:38 +0900

    add put_error

--- src/utils.c
+++ src/utils.c
@@ -1,14 +1,16 @@
-#includ "../include/philo.h"
+#include "../include/philo.h"
 
 size_t	ft_strlen(char *str)
 {
 	size_t i;
+
+	i = 0;
 	while (str[i] != '\0')
 		i++;
 	return (i);
 }
 
-void put_endl_fd(char *str, int fd)
+void ft_putendl_fd(char *str, int fd)
 {
 	write(fd, str, ft_strlen(str));
 	return ;
