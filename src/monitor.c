a18b858d005cfb646816b3810c0b9ef806fa8b60
hohnuki <ohnukihiroki8585@yahoo.co.jp>
2022-05-14 18:01:23 +0900

    add explain monitor

--- src/monitor.c
+++ src/monitor.c
@@ -2,5 +2,15 @@
 
 void	*monitor_routine(void *ptr)
 {
+	t_monitor	*monitor;
+
+	monitor = (t_monitor *)ptr;
+	while (1) {
+		//死んでから10ms以内に見つければいいからusleep(1000) or time_keeper(monitor->data, 1000)をする。
+		//mutexでロックする。
+		//get_timeから最後に食事した時間を引いて、time_to_dieと比較する。
+		//死んでたら、mutex_unlockして、print_actionして、終了。
+		//死んでなかったら、mutex_unlockして、何もしない。
+	}
 	return (NULL);
 }
